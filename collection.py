dbebe8da32e3c80a2dbb2c3f8420bfefad2a43d8
Moses <mosesejele20@gmail.com>
2022-12-01 14:56:40 +0000

    agparse & collections

--- collection.py
+++ collection.py
@@ -56,3 +56,82 @@ print(counter.subtract(counter2)) # output: None
 
 print(counter)# output: Counter({'apple': 1, 'cherry': 1, 'lemon': 1, 'mango': 1, 'pawpaw': 1, 'banana': 0, 'berries': 0})
 
+
+
+
+
+'''
+from the collections module defaultdict a subclass of dictionary take in int, list, or a function as an argument called default factory it is based on this default_factory that the defaultdict will be used 
+'''
+
+
+from collections import defaultdict
+sentence = "The red fox jumped over the fence and ran to the zoo for food"
+words = sentence.split(' ')
+d = defaultdict(int)
+for word in words:
+    d[word] +=1
+print(d) # output: defaultdict(<class 'int'>, {'The': 1, 'red': 1, 'fox': 1, 'jumped': 1, 'over': 1, 'the': 2, 'fence': 1, 'and': 1, 'ran': 1, 'to': 1, 'zoo': 1, 'for': 1, 'food': 1})
+
+# the default_factory as a list
+
+my_list = [(1234, 100.23), (345, 10.45), (1234, 75.00),
+        (345, 222.66), (678, 300.25), (1234, 35.67)]
+
+dl = defaultdict(list)
+
+for acct, amount in my_list:
+    dl[acct].append(amount)
+    
+print(dl) # output: defaultdict(<class 'list'>, {1234: [100.23, 75.0, 35.67], 345: [10.45, 222.66], 678: [300.25]})
+
+# lambda as default_factory:
+
+animal = defaultdict(lambda:'Monkey')
+animal['slow']= 'Snail'
+animal['fast']= 'Tiger'
+print(animal['tricky']) # output: Monkey
+print(animal['jumpy']) # output: Monkey
+
+
+
+
+'''
+Deque pronounced deck is another method from the collections module to create an instance of deque it takes in a list
+with deque you can append to the left rotate the list by indicating the number of elements you want to rotate
+'''
+
+from collections import deque
+juices = ["apple", "banana", "cherry", "apple", "cherry", "lemon", "mango", "pawpaw", "berries", ]
+deck = deque(juices)
+
+deck.append('smoothie')
+print(deck) # output: deque(['apple', 'banana', 'cherry', 'apple', 'cherry', 'lemon', 'mango', 'pawpaw', 'berries', 'smoothie'])
+    
+deck.appendleft('pepper')
+
+print(deck) # output: deque(['apple', 'banana', 'cherry', 'apple', 'cherry', 'lemon', 'mango', 'pawpaw', 'berries', 'smoothie'])
+
+deck.rotate(-1)
+
+print(deck) # output: deque(['apple', 'banana', 'cherry', 'apple', 'cherry', 'lemon', 'mango', 'pawpaw', 'berries', 'smoothie', 'pepper'])
+
+'''
+We can also use deque to instruct our programme to keep a certain no of value in it list.
+
+'''
+
+def get_text(filename, n=5):
+    try:
+        with open(filename, 'r') as f:
+            return deque(f, n)
+    except OSError:
+        print('Error opening file {}'.format(filename))
+        raise
+
+result = get_text('text.txt')
+print(result) 
+'''
+output: deque(["Which, but their children's end, nought could remove,\n", "Is now the two hours' traffic of our stage;\n", 'The which if you with patient ears attend,\n', 'What here shall miss, our toil shall strive to mend.\n', 'SCENE I. Verona. A public place.'], maxlen=5)
+'''
+
